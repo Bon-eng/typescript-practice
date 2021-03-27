d73c84d4715d8cd3d34fbbb96a9f6ce64b4bbbac
Bon-eng <keitaymzk@gmail.com>
2021-03-27 21:27:46 +0900

    new file11

---
+++ src/my-first-class.ts
@@ -0,0 +1,18 @@
+export {};
+//constructorの戻り値には型を指定してはいけないルールになっている(引数には必要)
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  profile (): string {
+    return `name: ${this.name}, age: ${this.age}`;
+  }
+}
+let taro = new Person('Taro', 30);
+console.log(taro.profile());
+// let hanako = new Person();
