6a73f41531bbc36b1b6d1bcfb0ae70dfcd3309e5
Bon-eng <keitaymzk@gmail.com>
2021-03-27 17:40:22 +0900

    new file

---
+++ src/object.ts
@@ -0,0 +1,10 @@
+export {};
+
+let profile1: object = { name: 'Pome' };
+profile1 = { birthYear: 1999 };
+
+let profile2: {
+  name: string;
+} = { name: 'Pome' };
+profile2 = { name: 'Osyushi'};
+7840
---
+++ src/union.ts
@@ -0,0 +1,5 @@
+export {};
+// 「|」は「or」のような役割になる。number型もしくはstring型という意味
+
+let value: number | string = 1;
+value = 'foo';
