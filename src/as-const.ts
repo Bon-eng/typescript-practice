8adf201bfad12799bf50eef37134323e6420a880
Bon-eng <keitaymzk@gmail.com>
2021-03-28 17:52:06 +0900

    new file 22

---
+++ src/as-const.ts
@@ -0,0 +1,17 @@
+export {};
+
+let name = 'Pome';
+
+name = 'ham';
+
+let nickname = 'ham' as const;
+nickname = 'ham';
+
+//as constを記述するだけでreadonlyというプロパティを追加することができる
+let profile = {
+  name: 'Pome',
+  height: 100
+} as const;
+
+profile.name = 'ham';
+profile.height = 200;
--- src/type-assertions.ts
+++ src/type-assertions.ts
@@ -5,6 +5,7 @@ let name: any = 'Pome';
 //記述方法は3タイプ
 let length = name.length as number;
 let length = (name as string).length;
+
 //⬇️はReactの記法であるJSXと似ているため、記述の場合はasを使うようにすること
 let length = (<string>name).length;
 
